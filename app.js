3df0a381cef5b58834b08397e99c936d36c26299
Akeel Alexander <alexander_akeel@hotmail.com>
2023-12-26 15:58:01 -0400

    Defining Error Template

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const engine = require('ejs-mate');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/Express-Error');
+
 
 main()
 .then(()=>{console.log("MONGO CONNECTION OPEN!!!")})
@@ -37,44 +40,55 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync( async (req, res) => {
     const Campgrounds = await Campground.find({});
     res.render('campgrounds/index', {Campgrounds});
-})
+}))
 
 app.get('/campgrounds/new', (req, res) =>{
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', catchAsync(async (req, res) => {
+    if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     // res.send(req.body);
     await campground.save();
     // res.redirect(`/campground/${campground._id}`);
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', {campground});
-})
+}))
 
-app.get('/campgrounds/:id/edit', async (req, res) =>{
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) =>{
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', {campground});
-})
+}))
 
-app.put('/campgrounds/:id', async (req, res) =>{
+app.put('/campgrounds/:id', catchAsync(async (req, res) =>{
     const {id} = req.params;
     const camp = await Campground.findByIdAndUpdate(id, {...req.body.campground}, {new:true});
     res.redirect(`/campgrounds/${camp._id}`);
-})
+}))
 
-app.delete('/campgrounds/:id', async (req, res) =>{
+app.delete('/campgrounds/:id', catchAsync (async (req, res) =>{
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Note Found', 404));
 })
+
+app.use((err, req, res, next) => {
+    const {statusCode = 500} = err;
+    if (!err.message) err.message = 'Oh no, Something Went Wrong!';
+    res.status(statusCode).render('error' , {err});
+});
 
 app.listen(3000, () =>{
     console.log("Serving on port 3000");
